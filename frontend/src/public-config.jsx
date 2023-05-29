30a4641ae98d67d67d78e9140c982dc077771433
Justin <42555501+mountainhokie@users.noreply.github.com>
2023-05-29 19:31:36 -0400

    Vercel Config

--- frontend/src/public-config.jsx
+++ frontend/src/public-config.jsx
@@ -1,4 +1,4 @@
 //export const PORT = import.meta.env.PORT ?? "8080";
-export const HOST = import.meta.env.VITE_HOST ?? "localhost:8080";
-export const API_SERVER_URL = `http://${HOST}/api`;
+export const HOST = import.meta.env.VITE_HOST ?? "http://localhost:8080";
+export const API_SERVER_URL = `${HOST}/api`;
 console.log(API_SERVER_URL);
