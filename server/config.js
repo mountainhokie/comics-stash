58c7a4efc1f18da021ccd163460374bc6eba9e60
Justin <42555501+mountainhokie@users.noreply.github.com>
2023-05-29 17:02:19 -0400

    Vercel Setup

--- server/config.js
+++ server/config.js
@@ -1,10 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const env = process.env;
 
 const PORT = env.PORT || "8080";
 const HOST = env.HOST || "localhost";
 const SERVER_URL = `http://${HOST}:${PORT}`;
 const MONGODB_URI = env.MONGODB_URL || "mongodb://localhost:27017";
-
 const DATABASE_NAME = env.DATABASE_NAME || "comics-stash";
 
 module.exports = {
